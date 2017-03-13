82a3122fddef3b69a9dadad4f61a6782278f32b1
mmuchai <muchaimercyw@gmail.com>
2017-03-13 23:56:45 +0300

    Fix errors on database

--- database.py
+++ database.py
@@ -1,20 +1,18 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
- 
-class Notes():
+
+Base = declarative_base()
+engine = create_engine('sqlite:///note_taking.db')
+
+
+class Notes(Base):
 
     __tablename__ = 'notes'
     #Table of the notes, containing IDs, content and status
 
     id = Column(Integer, primary_key=True)
     note_content = Column(String(20), nullable=False)
-    
-    def __init__ (self):
-    	self.create = str('')
+   
 
-
-
-
-
-
+Base.metadata.create_all(engine)
