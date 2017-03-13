2814c7a4577525cd4ed63736a3181afa663651d9
mmuchai <muchaimercyw@gmail.com>
2017-03-14 00:15:23 +0300

    note_taking.db

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
