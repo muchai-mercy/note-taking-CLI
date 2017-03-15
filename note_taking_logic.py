116c93f945a14a72183c5b0f6d364f1f6e49fd2f
mmuchai <muchaimercyw@gmail.com>
2017-03-15 11:03:01 +0300

    (Fix) Search query return dict of notes

--- note_taking_logic.py
+++ note_taking_logic.py
@@ -1,57 +1,53 @@
 from sqlalchemy.orm import Session
 from database import Notes, create_db, Base
-
+engine = create_db()
+Base.metadata.bind = engine
+session = Session()
 class Note():
 
 	def __init__ (self, note_content, note_id):
 		self.note_content = note_content
 		self.note_id = note_id
 	
+	
+	"""Create a new note and store it in database"""
 	def create_note(note_content):
 		new_note = Notes(note_content = note_content)
-		engine = create_db()
-		Base.metadata.bind = engine
-		session = Session()
 		session.add(new_note)
 		session.commit()
-	
+
+	"""View any note created using the Id"""
 	def view_note(note_id):
-		session = Session()
 		result = session.query(Notes).filter_by(note_id = note_id).first()
 		print ("Id: " + str(result.note_id) + " Content: " + result.note_content)
 
+	""" View a formated list of all notes"""
 	def view_all_notes():
-		session = Session()
 		result = session.query(Notes).all()
 		print(result)
 		for item in result:
 			print ("Id: " + str(item.note_id) + " Content: " + item.note_content)
 
+	"""Delete a note. Filter note to delete using note ID"""
 	def delete_note(note_id):
-		session = Session()
 		note_to_delete = session.query(Notes).filter_by(note_id = note_id).first()
 		deleted = session.delete(note_to_delete)
 		print("Id " + str(note_to_delete.note_id) + " deleted!")
 
+	"""Search for all notes that include a queried text
+		Filter all notes with text
+		Return a list of all notes that have text"""
 	def search_note(note_content):
-		session = Session()
 		results = session.query(Notes).filter_by(note_content = note_content).all()
-		note_list = []
-
+		note_list = {}
 		for result in results:
-			search_query = note_list.append(result.note_content)
-			for item in note_list:
-				print (note_list)
-		# if note_content['<text>'] in item:
-				# 	print ('Okay')
+			note_list[result.note_id] = result.note_content
+		print(note_list)
 
-Note.create_note('lfuhkf;jpi;')
-
-Note.view_note(1)
 # Note.view_note(1)
-# print(note.note_content)
-Note.view_all_notes()
 
-Note.delete_note(2)
+# Note.view_all_notes()
 
-Note.search_note("lfuhkf;jpi;")
+# Note.delete_note(1)
+
+Note.search_note("Get")
